bca7c9ba58ad92d939bcff947112bb7ae1eadeec
Sameeha Afrulbasha <75040242+sameehaafr@users.noreply.github.com>
2021-11-14 13:59:50 -0500

    Added the define_discriminator method

--- src/gansKeras.py
+++ src/gansKeras.py
@@ -23,8 +23,22 @@ def main():
     
     pm = pretty_midi.PrettyMIDI(sample_file)
     make_generator_model() #call to make_generator_model which makes the GANS
-
-
+    
+def define_discriminator(in_shape = (106,106,1)):
+    #got this from a tutorial - we can change the parameters/what functions to add once we know more
+    model = Sequential()
+    model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same', input_shape=in_shape))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(64, (3,3), strides=(2, 2), padding='same'))
+    model.add(LeakyReLU(alpha=0.2))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(BatchNormalization())
+    model.add(Dense(1, activation='sigmoid'))
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+    return model
 
 def make_generator_model():
     model = tf.keras.Sequential() # makes a sequential model using keras
