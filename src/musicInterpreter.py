b747baf97cae62dd31fc4c917c9f3bbaebcf5a1a
JonahLurton <lobsterlick@gmail.com>
2021-12-05 14:11:27 -0500

    Chords updates!
    Update with working Chord system. Has two known issues commented at the top detailing some weirdness with how chords are stored and my planned improvements.

--- src/musicInterpreter.py
+++ src/musicInterpreter.py
@@ -1,6 +1,18 @@
 from music21 import *
 from pathlib import Path
 
+#KNOWN PROBLEMS:
+#If two notes occur in the same time slot,
+#such as two 32nd notes when we are rounding to the nearest sixteenth
+#Then it will count them as two sixteenth notes in a row
+#I am going to make it count them as a sixteenth note chord of both of them.
+
+#Because it currently uses lists, which are ordered, to keep track of the pitches in a chord,
+#it will sometimes store a chord in the chord list multiple times, such as
+#{55, 75}, {75, 55}
+
+#Both of these problems should be fixed in the next version of this
+
 musicdir = "C:\\Users\\Lobst\\Documents\\AIMusicInterpreter\\maestro-v3.0.0\\2018"  #change this later! Will not work off Jonah's Computer!
 
 files = Path(musicdir)
@@ -9,37 +21,46 @@ STEP = 4 #This number is our "step". 1 = round to nearest quarter note. 2 = eigh
 pitchList = []
 durationList = []
 listIndex = 0
+chordList = [] 
 
 for file in files.iterdir(): #iterate through all the files
     currentSongStream = converter.parse(file).flatten() #convert the file to a useable music21 Stream
     #print(file)
     lastOffset = listIndex
     for noteOrChord in currentSongStream.iter().notes: #iterate through all the notes and chords
-        if isinstance(noteOrChord, note.Note): #Check if its a note or a chord. Currently just ignoring chords until i setup the chord to pitch ID system
-            noteOffset = int(round(noteOrChord.offset * STEP))
-            noteDuration = int(round(noteOrChord.duration.quarterLength * STEP)) #Once I have the chord system working, when two notes evaluate to the same offset, i will treat them as a chord
+        noteOffset = int(round(noteOrChord.offset * STEP))
+        noteDuration = int(round(noteOrChord.duration.quarterLength * STEP))
+        notePitch = 0
+        if isinstance(noteOrChord, note.Note): 
             notePitch = noteOrChord.pitch.midi - 11
-
-            while (noteOffset - lastOffset == 1 or noteOffset - lastOffset == 0) != True:
-                #print("noteOffset off of lastOffset by " + str(noteOffset - lastOffset))
-                pitchList.append(0)
-                durationList.append(0)
-                lastOffset += 1
-
-            #print("Appending note")
-            #print("Offset: " + str(noteOffset) + ", Duration: " + str(noteDuration) + ", Pitch: " + str(notePitch))
-            #print("Standard format: " + str(noteOrChord.nameWithOctave))
-
-            if noteOffset - lastOffset == 0:
-                #print("Double offset detected at " + str(noteOffset))
-                pitchList[noteOffset - 1] = notePitch
-                durationList[noteOffset - 1] = noteDuration
-            else:
-                pitchList.append(notePitch)
-                durationList.append(noteDuration)
+            #TODO: treat this chord and the note already there as a chord
+        else: #What to do if it's a chord
+            notePitches = []
+            for chordNote in noteOrChord.notes:
+                notePitches.append(chordNote.pitch.midi - 11)
+            #Now we have the list of pitches in the chord
+            if (notePitches in chordList) == False: #if we don't already have an ID for this set of pitches
+                chordList.append(notePitches)       #Then add it
+            notePitch = chordList.index(notePitches) + 128
             
-            lastOffset = noteOffset
+        while noteOffset - lastOffset > 1: #fill in the gaps
+            #print("noteOffset off of lastOffset by " + str(noteOffset - lastOffset))
+            pitchList.append(0)
+            durationList.append(0)
+            lastOffset += 1
+            
+        #print("Appending note")
+        #print("Offset: " + str(noteOffset) + ", Duration: " + str(noteDuration) + ", Pitch: " + str(notePitch))
+        #print("Standard format: " + str(noteOrChord.nameWithOctave))
+        
+        pitchList.append(notePitch)
+        durationList.append(noteDuration)
+            
+        lastOffset = noteOffset
     listIndex += lastOffset
     break #This to to make it only parse one song. The first one is ten minutes long, more than enough for initial testing.
 allNotes = list(zip(pitchList, durationList))
+print(chordList)
+print("\n")
 print(allNotes)
+
